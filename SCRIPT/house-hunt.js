5f189b7a65052ab6540b90b198dc971d26bf815d
dennomel <drkdennomel.gmail.com>
2025-02-07 00:51:38 +0300

    added commad to get form values

--- SCRIPT/house-hunt.js
+++ SCRIPT/house-hunt.js
@@ -6,3 +6,8 @@ document.addEventListener("DOMContentLoaded", function () {
   
   //stop default form submission
   form.addEventListener("submit",function(event) {event.preventDefault();
+
+//Get form values
+const name=document.getElementById("Name").ariaValueMax.trim();
+const name=document.getElementById("Email").ariaValueMax.trim();
+const name=document.getElementById("phone").ariaValueMax.trim();
